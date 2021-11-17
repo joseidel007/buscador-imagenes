e01e20ec462f31a2b94f3c90c43c49c3b7e99801
root <root@jp18cxe.fcld.acl>
2021-11-17 00:19:20 -0400

    Finished Aplication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import Buscador from './components/Buscador';
+import Resultado from './components/Resultado';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class  App extends React.Component {
+
+
+  state = {
+    termino: '',
+    imagenes: [],
+    pagina: ''
+  }
+
+  scroll = () =>{
+    const elemento = document.querySelector('.jumbotron');
+    elemento.scrollIntoView('smooth', 'start');
+  }
+
+  paginaSiguiente = () =>{
+    //Leer el state de la pagina actual
+    let pagina = this.state.pagina;
+
+    //Sumar 1 al State Actual
+    pagina += 1;
+    
+
+    //Agregarr cambio al State
+    this.setState({
+      pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    })
+
+    console.log(pagina);
+  }
+
+  paginaAnterior = () =>{
+    //Leer state de la pagina actual
+    let pagina = this.state.pagina;
+
+
+    //Validar si la pagina es 1 devolver null
+    if(pagina === 1 ) return null;
+
+    //Restar 1 a la pagina actual
+    pagina -= 1;
+
+    //Agregar cambio al State
+    this.setState({
+      pagina
+    }, ()=> {
+      this.consultarApi();
+      this.scroll();
+    })
+    console.log(pagina)
+  }
+
+  consultarApi = () =>{
+    const termino = this.state.termino;
+    const pagina = this.state.pagina;
+    const url = `https://pixabay.com/api/?key=24359434-0f731b848c85bb77e34871aa6&q=${termino}&per_page=20&page=${pagina}`;
+
+    fetch(url)
+    .then(respuesta => respuesta.json() )
+    .then(resultado => this.setState({imagenes: resultado.hits}) )
+  }
+  //Esta funcion es llamada desde el Buscador el cual le pasa los datos del input
+  datosBusqueda = (termino) =>{
+    this.setState({
+      termino: termino,
+      pagina: 1
+    }, () =>{
+      this.consultarApi();
+    } )
+  }
+
+  
+
+
+  render() { 
+    return (
+      <div className="app container">
+            <div className="jumbotron">
+                <p className="lead text-center">Buscador De Imágenes</p>
+
+                {/* Se llama al componente y se le pasa un valor con una funcion */}
+                <Buscador 
+                    datosBusqueda = {this.datosBusqueda} />
+            </div>
+
+            <div className="row justify-content-center">
+              <Resultado 
+                imagenes = {this.state.imagenes} 
+                paginaAnterior = {this.paginaAnterior}
+                paginaSiguiente = {this.paginaSiguiente}/>
+              
+            </div>  
+
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
---
+++ src/components/Buscador.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+
+class Buscador extends React.Component {
+
+    
+    busquedaRef = React.createRef();
+
+    //Es llamada desde el onSubmit para obtener los datos que introduzcan en el formulario
+    obtenerDatos = (e) =>{
+        e.preventDefault();
+
+        //Tomamos el valor del Input 
+        const termino = this.busquedaRef.current.value
+
+        //Enviamos el valor guardado termino y lo enviamos al componente principa;
+        this.props.datosBusqueda(termino);
+    }
+    
+    render() { 
+        return (
+            <form onSubmit={this.obtenerDatos}>
+                <div className="row">
+                    <div className="form-group col-md-10">
+                        <input ref={this.busquedaRef} type="text" className="form-control form-control-lg" 
+                            placeholder="Busca tu imagen. Ejemplo: Paisaje"/>
+                    </div>
+                    <div className="form-group col-md-2">
+                        <input type="submit" className="btn btn-lg btn-danger btn-block" value="Buscar..."/>
+                    </div>
+                </div>
+            </form>
+        );
+    }
+}
+ 
+export default Buscador;
